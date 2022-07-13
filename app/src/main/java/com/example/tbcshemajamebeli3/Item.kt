75835ef2f11bfe6235d55bb5f98a2c5673084088
Tengiz <Tengiz152@gmail.com>
2022-07-13 11:06:28 +0400

    created app

---
+++ app/src/main/java/com/example/tbcshemajamebeli3/Item.kt
@@ -0,0 +1,14 @@
+//package com.example.tbcshemajamebeli3
+//
+//data class Item (
+//    val name: String,
+//    val starticon: Int,
+//    val endicon: Int
+//    )
+//
+//val Items = mutableListOf<Item>(
+//    Item("Edit Profile",  R.drawable.user, R.drawable.ic_end_button),
+//    Item("Address",  R.drawable.adress, R.drawable.ic_end_button),
+//    Item("Notifilication",  R.drawable.notification,  R.drawable.ic_end_button),
+//    Item("Payment",  R.drawable.payment,  R.drawable.ic_end_button)
+//)
